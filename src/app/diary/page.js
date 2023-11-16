c96f59cbf8ed96389f4e962cac150448e14e972e
Rendi Wijaya <wijayarendi9@gmail.com>
2023-11-16 18:42:15 +0800

    fetch API
    displays the contents of the diary using the API from mockapi.io

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -1,10 +1,46 @@
+"use client";
+import "@/styles/diary.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Diary() {
-    return(
-        <>
-        <div className="diary-container">
-            <h1>Seninku yang bahagia</h1>
-            <p className="p-diary">Today was a vibrant tapestry of experiences. The morning sun ushered in a sense of optimism, setting the tone for a productive day. I navigated through a bustling cityscape, the rhythm of the streets harmonizing with my footsteps. Work was a whirlwind of challenges and accomplishments, each task a stepping stone towards personal and professional growth. During lunch, I took a moment to recharge in a cozy cafe, savoring the flavors of a comforting meal. The afternoon unfolded with a blend of meetings and creative endeavors, pushing the boundaries of my skills. As the day waned, a breathtaking sunset painted the sky in hues of pink and orange, a serene backdrop to reflective moments. Now, as I unwind, I find solace in the simple joys that adorned this day, grateful for the rich mosaic of experiences that made it uniquely mine.</p>
-        </div>
-        </>
-    )
+  const [judul, setJudul] = useState([]);
+  const [isiDiary, setIsiDiary] = useState([]);
+
+  const endpointAPI = "https://6555efd084b36e3a431eacd7.mockapi.io/rendi/api/v1/diary";
+
+  async function getDiary() {
+    const res = await axios.get(endpointAPI);
+    const data = res.data;
+
+    const judul = data.map((item) => item.judul);
+    setJudul(judul);
+
+    const isi_diary = data.map((item) => item.isi_diary);
+    setIsiDiary(isi_diary);
+  }
+
+  useEffect(() => {
+    getDiary();
+  }, []);
+
+  return (
+    <div>
+      {judul.length > 0 ? (
+        <ul>
+          {judul.map((item, idx) => (
+              <li>
+                <div className="diary-container">
+                    <h1>{judul[idx]}</h1>
+                    <p className="p-diary">{isiDiary[idx]}</p>
+                </div>
+              </li>
+
+          ))}
+        </ul>
+      ) : (
+        "API not loading"
+      )}
+    </div>
+  );
 }
